e39ff246114e6dc237ac1571d89fde5f3647eb10
David-The-Programmer <davhuzh@gmail.com>
2022-06-18 16:32:29 +0800

    [feat] Added speed up of falling after level up

--- ui/ui.go
+++ ui/ui.go
@@ -7,19 +7,22 @@ import (
 )
 
 type ui struct {
-	engine   gotetromino.Engine
-	user     gotetromino.User
-	renderer gotetromino.Renderer
-	ticker   *time.Ticker
+	engine       gotetromino.Engine
+	user         gotetromino.User
+	renderer     gotetromino.Renderer
+	ticker       *time.Ticker
+	tickDuration time.Duration
 }
 
 func New(e gotetromino.Engine, r gotetromino.Renderer, u gotetromino.User) gotetromino.UI {
-	t := time.NewTicker(800 * time.Millisecond)
+	d := 800 * time.Millisecond
+	t := time.NewTicker(d)
 	var ui gotetromino.UI = &ui{
-		engine:   e,
-		user:     u,
-		renderer: r,
-		ticker:   t,
+		engine:       e,
+		user:         u,
+		renderer:     r,
+		ticker:       t,
+		tickDuration: d,
 	}
 	return ui
 }
@@ -27,8 +30,15 @@ func New(e gotetromino.Engine, r gotetromino.Renderer, u gotetromino.User) gotet
 func (ui *ui) Run() {
 	actions := ui.user.Action()
 	interactions := ui.user.Interaction()
+	state := ui.engine.State()
 	for {
-		ui.renderer.Render(ui.engine.State())
+		newState := ui.engine.State()
+		if newState.Level-state.Level > 0 {
+			ui.tickDuration = ui.tickDuration - time.Duration(newState.Level*int(ui.tickDuration/10))
+			ui.ticker.Reset(ui.tickDuration)
+		}
+		state = newState
+		ui.renderer.Render(state)
 		select {
 		case i := <-interactions:
 			if i == gotetromino.Exit {
