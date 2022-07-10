908ac003b3ec61a04385a7dc59e8e9743a17fb78
David-The-Programmer <davhuzh@gmail.com>
2022-07-10 12:26:14 +0800

    [ref] Moved all app related code into one file

--- ui/app.go
+++ ui/app.go
@@ -80,3 +80,17 @@ func (a *app) Run() {
 	}
 
 }
+
+func (a *app) render(s gotetromino.State) {
+	a.renderer.SetState(s)
+	a.renderer.Render()
+
+	if s.ClearedPrevLevel {
+		// make animation faster as more levels are cleared
+		// TODO: Refactor limiting animation speed
+		if a.tickDuration > 80*time.Millisecond {
+			a.tickDuration = a.tickDuration - (80 * time.Millisecond)
+			a.ticker.Reset(a.tickDuration)
+		}
+	}
+}
